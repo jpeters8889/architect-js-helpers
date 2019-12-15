5373ed81b5bd54237b09d200fc462aa0f69b214a
jamie peters <jamie.phpeters@gmail.com>
2019-12-15 15:08:41 +0000

    plan helper updates

--- src/traits/IsAFormField.js
+++ src/traits/IsAFormField.js
@@ -18,13 +18,15 @@ export default {
             this.actualValue = this.value;
         }
 
-        window.Architect.$on(this.listener, () => {
-            window.Architect.$emit(this.emitter, this.getFormData());
-        });
+        this.bootstrapListeners();
+
+        this.debouncedEvents = window._.debounce(this.dispatchEvents, 500);
     },
 
     data: () => ({
         actualValue: '',
+        emitterValue: null,
+        setEmitterValue: true,
     }),
 
     methods: {
@@ -34,5 +36,58 @@ export default {
                 value: this.actualValue,
             };
         },
-    }
+
+        dispatchEvents() {
+            if (this.emitterValue) {
+                window.Architect.$emit(this.name + '-changed', this.emitterValue);
+            }
+        },
+
+        bootstrapListeners() {
+            window.Architect.$on(this.listener, () => {
+                window.Architect.$emit(this.emitter, this.getFormData());
+            });
+
+            /**
+             * listeners [
+             *      changed: [
+             *          column1
+             *          column2
+             *      ]
+             *  ]
+             */
+
+            Object.keys(this.metas.listeners).forEach((event) => {
+                let column = this.metas.listeners[event];
+
+                if (typeof column === 'string') {
+                    window.Architect.$on(column + '-' + event, (value) => {
+                        Architect.request().post('/listener', {
+                            blueprint: this.$route.params.blueprint,
+                            event: column + '-' + event,
+                            column: this.name,
+                            value: JSON.stringify(value)
+                        }).then((response) => {
+                            this.actualValue = response.data;
+                        }).catch(error => {
+                            Architect.$emit(error.response.data.message);
+                        });
+                    });
+                }
+            });
+        },
+    },
+
+    watch: {
+        emitterValue: function (newValue) {
+            if (newValue !== '') {
+                this.debouncedEvents();
+            }
+        },
+        actualValue: function (newValue) {
+            if (this.setEmitterValue) {
+                this.emitterValue = newValue;
+            }
+        }
+    },
 }
